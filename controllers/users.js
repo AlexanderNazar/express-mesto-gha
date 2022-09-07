886ae4af54c2d6d0490c8b646d620d8e11768cdf
Alexander Nazarov <naa@oplot48.ru>
2022-09-07 15:44:03 +0300

    fix: 2 поправки для тестов 14 ПР

--- controllers/users.js
+++ controllers/users.js
@@ -54,7 +54,15 @@ const createUser = (req, res, next) => {
       User.create({
         name, about, avatar, email, password: hash,
       })
-        .then((user) => res.status(201).send({ data: user }))
+        .then((user) => res.status(201).send({
+          user: {
+            name: user.name,
+            about: user.about,
+            avatar: user.avatar,
+            email: user.email,
+            _id: user._id,
+          },
+        }))
         .catch((err) => {
           if (err.code === 11000) {
             next(new ConflictError('Пользователь с таким Email уже существует'));
