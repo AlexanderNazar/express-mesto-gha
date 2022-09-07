4a2b01a751e7f4a139c81a2679748a2d88541a9a
Alexander Nazarov <naa@oplot48.ru>
2022-09-07 12:43:32 +0300

    feat: сдача на первое ревью 14 ПР

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,32 @@
 const router = require('express').Router();
 
+const { celebrate, Joi } = require('celebrate');
+
 const {
-  getUsers, getUserOnId, createUser, updateUser, updateAvatar,
+  getUsers, getUserOnId, updateUser, updateAvatar, getMyUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 
-router.get('/:userId', getUserOnId);
+router.get('/me', getMyUser);
 
-router.post('/', createUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+}), getUserOnId);
 
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
 
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().regex(/https?:\/\/[-/\w./~^:?#!@$&'()*+,;=\][]*/),
+  }),
+}), updateAvatar);
 
 module.exports = router;
